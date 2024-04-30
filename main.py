1bbe3780ce7b947205412e3f365afdca48e4d380
Samit-Singhvi <84736010+Samit-Singhvi@users.noreply.github.com>
2024-04-30 21:05:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import urllib.parse
-import requests
+
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
@@ -14,44 +13,43 @@ def model_prediction(test_image):
     return np.argmax(predictions) #return index of max element
 
 #Sidebar
-def main():
-    st.sidebar.title("Dashboard")
-    app_mode = st.sidebar.selectbox("Select Page", ["Prediction"])
-    st.sidebar.image(r"C:\Users\Hp\Downloads\logo1.jpg", width=100)
-    st.sidebar.title("Food Predictor")
-    st.sidebar.markdown("---")
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About Project","Prediction"])
 
-    # Prediction Page
-    if app_mode == "Prediction":
-        st.header("Model Prediction")
-        image_path = "home_img.jpg"
-        st.image(image_path)
+#Main Page
+if(app_mode=="Home"):
+    st.header("FRUITS & VEGETABLES RECOGNITION SYSTEM")
+    image_path = "home_img.jpg"
+    st.image(image_path)
 
-        test_image = st.file_uploader("Choose an Image:")
-        if st.button("Show Image"):
-            st.image(test_image, width=4, use_column_width=True)
+#About Project
+elif(app_mode=="About Project"):
+    st.header("About Project")
+    st.subheader("About Dataset")
+    st.text("This dataset contains images of the following food items:")
+    st.code("fruits- banana, apple, pear, grapes, orange, kiwi, watermelon, pomegranate, pineapple, mango.")
+    st.code("vegetables- cucumber, carrot, capsicum, onion, potato, lemon, tomato, raddish, beetroot, cabbage, lettuce, spinach, soy bean, cauliflower, bell pepper, chilli pepper, turnip, corn, sweetcorn, sweet potato, paprika, jalepeño, ginger, garlic, peas, eggplant.")
+    st.subheader("Content")
+    st.text("This dataset contains three folders:")
+    st.text("1. train (100 images each)")
+    st.text("2. test (10 images each)")
+    st.text("3. validation (10 images each)")
 
-        # Predict button
-        if st.button("Predict"):
-            st.write("Our Prediction")
-            result_index = model_prediction(test_image)
-
-            # Reading Labels
-            with open("labels.txt") as f:
-                content = f.readlines()
-            label = [i[:-1] for i in content]
-            prediction_result = label[result_index]
-
-            st.success("Model is Predicting it's a {}".format(prediction_result))
-
-            # Pass prediction result back to Django app
-            django_url = "http://localhost:8000/update_search_bar"  # Change URL accordingly
-            response = requests.post(django_url, data={"prediction": prediction_result})
-
-            if response.status_code == 200:
-                st.write("Prediction result sent back to Django web app successfully!")
-            else:
-                st.error("Failed to send prediction result to Django web app.")
-
-if __name__ == "__main__":
-    main()
+#Prediction Page
+elif(app_mode=="Prediction"):
+    st.header("Model Prediction")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        with open("labels.txt") as f:
+            content = f.readlines()
+        label = []
+        for i in content:
+            label.append(i[:-1])
+        st.success("Model is Predicting it's a {}".format(label[result_index]))
